0422db8c2aa5599e2d73cd4f0e9973e895273abd
William <william.kapke@gmail.com>
2015-06-18 04:20:49 +0000

    1.0.0

--- postals.js
+++ postals.js
@@ -1,6 +1,95 @@
 
-module.exports = {
-	get US(){
-		return require("./data/US.json");
-	}
+var tab = 0x09;
+var newline = 0x0A;
+
+function iterate(buffer, i){
+  i = i || 0;
+  var l = buffer.length;
+  while(buffer[l-1] === newline) l--;
+
+  var r = {
+    get newline(){
+      return i===0 || buffer[i-1] === newline;
+    },
+    next: function(match){
+      while(i <= l) {
+        var c = buffer[i++];
+        if(match(c))
+          break;
+      }
+      return i;
+    },
+    get eof(){
+      return i >= l;
+    }
+  };
+
+  function tab_nl(c){ return c === tab || c === newline }
+
+  Object.defineProperties(r.next, {
+    field: { get: function(){
+      var start = i;
+      var end = r.next(tab_nl)-1;
+      return buffer.toString('utf8', start, end);
+    }},
+    line: { value: function () {
+      while(i < l) {
+        var c = buffer[i++];
+        if(c === newline) return i;
+      }
+    }},
+    object: { get: function(){
+      if(r.eof) return;
+      if(!r.newline) throw new Error("Invalid starting point");
+      return {
+        country_code: this.field,
+        postal_code: this.field,
+        place_name: this.field,
+        admin_name1: this.field,
+        admin_code1: this.field,
+        admin_name2: this.field,
+        admin_code2: this.field,
+        admin_name3: this.field,
+        admin_code3: this.field,
+        latitude: this.field,
+        longitude: this.field,
+        accuracy: this.field
+      };
+    }}
+  });
+
+  return r;
 }
+
+
+
+exports = module.exports = {
+  get data() {
+    return require('fs').readFileSync(__dirname+'/allCountries.txt');
+  },
+  get index() {
+    return require('./index.json');
+  },
+  iterate: function(data, i){
+    return iterate(data || exports.data, i);
+  }
+};
+
+//add getters for the countries: e.g.: postals.US, postals.AU, ...etc
+Object.keys(exports.index).forEach(function (cc) {
+  Object.defineProperty(exports, cc, {
+    get: function(){
+      var reader = exports.iterate(null, exports.index[cc].start);
+      var next = reader.next;
+      var out = [];
+
+      while(true){
+        var obj = next.object;
+        if(!obj || obj.country_code!==cc) break;
+        out.push(obj);
+      }
+
+      return out;
+    }
+  });
+});
--- update.js
+++ update.js
@@ -1,5 +1,17 @@
+var postals = require('./');
 var fs = require("fs");
 var exec = require("child_process").exec;
+var assert = require('assert');
+
+var timeout = setTimeout(function () {
+  errd("Script timeout exceeded");
+}, 300000);//5 min
+
+function errd(err) {
+  clearTimeout(timeout);
+  console.error(err);
+  process.exit(1);
+}
 
 function pull(){
 	console.log('getting latest from https://github.com/storehours/postals');
@@ -7,7 +19,7 @@ function pull(){
 		'git config --global user.name William Kapke && ' +
 		'git config --global user.email william.kapke@gmail.com && ' +
 		'git pull https://github.com/storehours/postals.git master', function(err, stdout){
-		if(err) return console.error(err);
+		if(err) return errd(err);
 		console.log(stdout);
 		download();
 	});
@@ -15,88 +27,165 @@ function pull(){
 
 function download(){
 	console.log("downloading...");
-	exec("curl http://download.geonames.org/export/zip/US.zip > US.zip", function(err){
-		if(err) return console.error(err);
+	exec("curl http://download.geonames.org/export/zip/allCountries.zip > allCountries.zip", function(err){
+		if(err) return errd(err);
 
 		console.log("unzipping...");
-		exec("jar xf US.zip", function(err){
-			if(err) return console.error(err);
-			read();
+		exec("jar xf allCountries.zip", function(err){
+			if(err) return errd(err);
+      sort();
 		});
 	});
 }
 
-function read() {
-	var postals = {};
-	console.log("reading...");
-	var txt = fs.readFileSync("./US.txt", "utf8");
-	txt.split('\n').forEach(function(line){
-		if(!line) return;
-		var columns = line.split('\t');
-		postals[columns[1]] = [parseFloat(columns[9]), parseFloat(columns[10])];
-	});
-	format(postals);
+function sort(){
+  console.log('sorting the data');
+  var lines = [];
+  var data = postals.data;
+  var reader = postals.iterate(data);
+
+  var start = 0;
+  var tab = c => c===0x09;
+
+  do {
+    reader.next(tab); // read to end of country code
+    reader.next(tab); // read to end of postal code
+    reader.next(tab); // read to end of place name
+    reader.next(tab); // read to end of admin name1
+    reader.next(tab); // read to end of admin code1
+    reader.next(tab); // read to end of admin name2
+    reader.next(tab); // read to end of admin code2
+    reader.next(tab); // read to end of admin name3
+    var end = reader.next(tab); // read to end of admin code3
+
+    lines.push({
+      key: data.toString('utf8', start, end),
+      data: data.toString('utf8', start, (start = reader.next.line()))
+    });
+
+//    if(lines.length%10000===0) console.log(lines.length);
+  }
+  while(!reader.eof);
+
+  lines.sort((a, b) => a.key.localeCompare(b.key));
+
+  console.log('writing data back to disk');
+  var wstream = fs.createWriteStream(__dirname+'/allCountries.txt');
+  lines.forEach(l=> wstream.write(l.data));
+
+  wstream.on('finish', validate_diff);
+  wstream.end();
 }
 
+function validate_diff(){
+  console.log("ensure there weren't an abnormal amount of changes");
+  exec("git diff --stat ./allCountries.txt", function(err, stdout, stderr){
+    if(err) return errd(err);
+    if(stderr) return errd(stderr);
 
-function format(postals) {
-	console.log("formatting...");
-	//custom output format
-	var out = ['{'];
-	var keys = Object.keys(postals).sort(function(a,b){ return parseInt(a, 10) - parseInt(b, 10)});
-	var end = keys.length-1;
-	keys.forEach(function (postal, i) {
-		var line = '"'+postal+'": '+ JSON.stringify(postals[postal]);
-		if(i!=end)
-			line += ',';
-		out.push(line);
-	});
-	out.push('}');
+    if(stdout) {
+      console.log(stdout);
+      var insertions = /(\d+) insertions/.exec(stdout)[1];
+      var deletions = /(\d+) deletions/.exec(stdout)[1];
+      if(Math.max(insertions, deletions) > 100) return errd("Too many changes!");
 
-	save(out.join('\n'));
+      validate_data();
+    }
+    else {
+      console.log('No files changed');
+      process.exit();
+    }
+  });
 }
 
+function validate_data(){
+  console.log('validating data');
+  var reader = postals.iterate();
+  var field = 0;
+  var line = 0;
 
-function save(data){
-	console.log("saving...");
-	fs.writeFileSync("./data/US.json", data);
-	add();
+  //iterate the data and ensure all lines contain 12 fields & valid coords
+  while(!reader.eof) {
+    if(reader.newline) {
+      if(field % 12 !== 0) return errd('Invalid format on line ' + line);
+      line++;
+    }
+
+    var val = reader.next.field;
+    if(field===9 && !/^-?\d{1,2}\.\d+$/.test(val)) return errd('Invalid latitude on line ' + line);
+    if(field===10 && !/^-?\d{1,3}\.\d+$/.test(val)) return errd('Invalid longitude on line ' + line);
+
+    field++;
+  }
+
+  index();
 }
 
+function index(){
+  console.log('creating index');
+  var reader = postals.iterate();
+  var index = {};
+  var lineno = 0;
+
+  do{
+    var cc = reader.next.field;
+    if(!cc) return index;
+
+    var country = index[cc];
+    if(!country) {
+      index[cc] = { start: lineno, lines: 1 };
+    }
+    else {
+      country.lines ++;
+    }
+  }
+  while(lineno = reader.next.line());
+
+  fs.writeFileSync(__dirname+'/index.json', JSON.stringify(index, null, 2));
+
+  add();
+}
 
 function add(){
-	console.log("add...");
-	exec('git add -v ./data/US.json', function(err, stdout, stderr){
-		if(stderr) console.log(stderr);
-		if(err) return console.error(err);
+	console.log("adding local changes to git");
+
+	exec('git add -v ./allCountries.txt ./index.json', function(err, stdout, stderr){
+    if(err) return errd(err);
+    if(stderr) return errd(stderr);
+
 		if(stdout) {
 			commit();
 		}
 		else {
-			console.log("no changes.");
+      return errd('No changes? That\'s not right!');
 		}
 	});
 }
 
 function commit(){
-	console.log("commit...");
-	exec('git commit -m "auto update"', function(err, stdout, stderr){
-		if(stdout) console.log(stdout);
-		if(stderr) console.log(stderr);
-		if(err) return console.error(err);
+	console.log("commit changes to git");
+	exec('npm version patch --no-git-tag-version && git commit -m "auto update"', function(err, stdout, stderr){
+    if(err) return errd(err);
+    if(stderr) return errd(stderr);
+    if(stdout) console.log(stdout);
+
 		push();
 	});
 }
 
 function push(){
-	console.log("push...");
+	console.log("push to github");
 	exec('git push https://'+process.env.AUTH_TOKEN+'@github.com/storehours/postals.git', function(err, stdout, stderr){
-		if(err) return console.error(err);
-		if(stdout) console.log(stdout);
-		if(stderr) console.log(stderr);
+		if(err) return errd(err);
+		if(stderr) return errd(stderr);
+    if(stdout) console.log(stdout);
 
 		console.log("done.");
+    process.exit();
 	});
 }
 
+//validate_diff();
+//sort();
+//download();
 pull();
